84a64f67d703295a052f2c7d1d493df989260d76
Daniel <danhu849@student.liu.se>
2023-01-14 12:28:22 +0100

    Can merge cells in excel files now

--- exceller.py
+++ exceller.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from openpyxl import Workbook
+import openpyxl
 import schedlr
 import json
 import os
@@ -15,17 +16,22 @@ import os
 
 
 def initialize_excel_file(filename='7_19.xlsx'):
-    wb = Workbook()
-    ws1 = wb.active
-    ws1.title = "Schema tdp007 och tdp019"
+    
     wb.save(filename=filename)
 
+
+def generate_xlsx_color_scheme(file_, start_cell):
+    for i in range(2, 5): 
+        ws.merge_cells(start_row=i, start_column='F', end_row=5, end_column='I')
+    
+    
 
 
 
 def main(filename = "tdp007_019_assistent_schema.xlsx", csv="tdp007_019.csv"):
     # Get correctly formated schedule info.
     formatted_csv = "7_19.csv"
+    xl = '7_19.xlsx' 
     schedlr.format_timeedit_categories(csv, formatted_csv)
     schedule_dict = schedlr.initialize_week_schedule(formatted_csv)
     #print(json.dumps(schedule_dict, indent=2))
@@ -33,7 +39,18 @@ def main(filename = "tdp007_019_assistent_schema.xlsx", csv="tdp007_019.csv"):
     # Create Excel file
     assistants = schedlr.get_assistants(formatted_csv, 'TDP007') 
     #print(assistants)
-    initialize_excel_file()
+
+
+    wb = Workbook()
+    ws1 = wb.active
+    ws1.title = "Schema tdp007 och tdp019"
+
+    for i in range(2, 5): 
+        ws1.merge_cells('F2:I2')
+#        ws1.merge_cells(start_row=i, start_column='F', end_row=5, end_column='I')
+
+    wb.save(filename=xl)
+    #initialize_excel_file()
     
     return "0"
 
